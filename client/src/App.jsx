188c4e290e7ec8b39fdd33ad710236f8f3156a74
tschowalter <139142650+tschowalter@users.noreply.github.com>
2024-05-20 22:18:10 -0400

    Added citations to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,10 @@
+/* Enormous citation for this MERN stack tutorial: https://www.youtube.com/watch?v=4sKkQ1WLHjQ&t=5434s&pp=ygUgY291ZGUgd2l0aCB5b3VzYWYgYm9va3N0b3JlIG1lcm4%3D
+Almost all of the code is either copied from this tutorial or was written using concepts that I learned from following along with the tutorial.
+You will see a commit called "debugged with chatGPT" — I used chatGPT two times while writing the code when I didn't understand why something wasn't working and I couldn't find answers on any forums to tell me what the problem was and what I could do to fix it. 
+However, both times, the answer ended up appearing later on in the tutorial, so I replaced the implementated suggested by chatGPT with what I was seeing in the tutorial. 
+Thus, all remaining code is either original, from the tutorial, or something I learned from a specific page/forum post which will be cited in an in-line comment.
+*/
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
